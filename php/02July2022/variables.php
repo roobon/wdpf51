02b090d80d2a1a8001a90b7f605c00394a2d6cba
roobon <roobon@gmail.com>
2022-07-02 19:00:33 +0600

    02 July files added

---
+++ php/02July2022/variables.php
@@ -0,0 +1,26 @@
+<?php 
+
+    //variables 
+
+    $x = "Rahim";
+    $y = "Karim";
+    
+    echo "$x and $y are two brothers"; 
+    // We can put string and variables in one quataton
+            echo "<br>";
+    echo $x . " and " .  $y . " are two brothers";
+    // We can use concatation operator (.) 
+    echo "<br>";
+
+    $num1 = 100;
+    $num2 = 150;
+    
+    $res = $num2 + $num1;
+
+    echo "Sum of $num1 and $num2 is $res";
+    echo "<br>";
+    echo 'Sum of $num1 and $num2 is ' . $res;
+
+
+
+?>
