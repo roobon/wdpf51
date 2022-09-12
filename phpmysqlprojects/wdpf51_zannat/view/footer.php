2a495cf4fa1332e208a56ff35d6a53f594749fff
roobon <roobon@gmail.com>
2022-09-12 19:17:45 +0600

    12 September files added

---
+++ phpmysqlprojects/wdpf51_zannat/view/footer.php
@@ -0,0 +1,9 @@
+	<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> 1.0.2
+        </div>
+        <strong>Copyright &copy; 2018-2019 <a href="">ILovePrograming</a>.</strong> All rights reserved.
+     </footer>
+      
+  </body>
+</html>
